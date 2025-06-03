60b7f34c7c7750be289d44f88f2f4dfc1961bd9f
DEV2A-28 <guilhermeykgg@gmail.com>
2025-06-03 09:48:52 -0300

    codigos.js

---
+++ variaveis/codigos.js
@@ -0,0 +1,18 @@
+let nome = &quot;Guilherme&quot;
+/*console.log(nome)
+let idade = &quot;18&quot;
+console.log(idade)
+let apredendoJavaScript = &quot;true&quot;
+console.log(aprendendoScript)*/
+/*let nome = &quot;Guilherme&quot;
+let sobrenome = &quot;costa&quot;
+let nomeCompleto =(nome*sobrenome)
+console.log (nomeCompleto)
+const num1 = 18
+const num2 = 37
+console.log(num1+num2)/*
+/*cons cidade = &quot;ferraz&quot;
+const habitantes = 3000
+console.log (&quot;Em&quot; ,cidace, &quot;tem&quot; ,habitantes, &quot;habitantes&quot;)
+let preco = 69
+preco = 69
